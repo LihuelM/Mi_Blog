82f4b101c81a79ced0faae8e248047c79dadde86
Lihuel <muracciole.al@gmail.com>
2022-10-17 16:11:08 +0700

    Primer estructura

---
+++ animaciones.js
@@ -0,0 +1,5 @@
+// Animación de parrafo de Bienvenida
+
+new TypeIt("#myElement", {
+    strings: "Bienvenidos a mi blog! A traves de él, espero que puedas conocer un poco más de mi y de mis proyectos...",
+  }).go();
